53f0dcb2ea94a57c322a323fbabef32a19e1ae83
tkreyche <tkreyche@well.com>
2025-11-06 16:52:35 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 
+"""
+I75W LED Matrix Display for the Raymarine Tiller Pilot
+https://github.com/tkreyche/I75W-Raymarine-Autopilot/blob/main/README.md
+
+This project uses custom software with off-the-shelf hardware to create an remote daylight-visible display,
+to make it easier to operate a Raymarine ST1000+ or ST2000+ Tiller Pilot.
+
+
+MIT License
+
+Copyright (c) 2025 Thomas Kreyche
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+"""
+
+
 # ============================================================================
 # IMPORTS
 # ============================================================================
